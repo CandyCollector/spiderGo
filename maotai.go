7baaddb9c64712147585fbd03c7351f06004b354
e <tuanzhang87@gmail.com>
2021-06-13 17:41:25 +0800

    修改豆瓣 URL 正则匹配规则

--- maotai.go
+++ maotai.go
@@ -5,31 +5,13 @@ import (
 
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/debug"
+	"github.com/gocolly/colly/extensions"
+	"time"
 )
 
-func main() {
-	url := "https://finance.sina.com.cn/realstock/company/sh600519/nc.shtml"
-
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Turn on asynchronous requests
-		colly.Async(),
-		// Attach a debugger to the collector
-		colly.Debugger(&debug.LogDebugger{}),
-	)
-
-	// Limit the number of threads started by colly to two
-	// when visiting links which domains' matches "*httpbin.*" glob
-	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*https.*",
-		Parallelism: 2,
-		//Delay:      5 * time.Second,
-	})
-
-	// Start scraping in five threads on 
-	for i := 0; i < 5; i++ {
-		c.Visit(fmt.Sprintf("%s?n=%d", url, i))
-	}
-	// Wait until threads are finished
-	c.Wait()
+func main(){
+	t := time.Now()
+	number := 1
+	c := colly.NewCollector()
+	
 }
