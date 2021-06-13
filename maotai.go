2ed8fffceaf64f759164fc51c07847f23ed6c6d2
e <tuanzhang87@gmail.com>
2021-06-13 16:54:29 +0800

    茅台测试

---
+++ maotai.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/debug"
+)
+
+func main() {
+	url := "https://finance.sina.com.cn/realstock/company/sh600519/nc.shtml"
+
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Turn on asynchronous requests
+		colly.Async(),
+		// Attach a debugger to the collector
+		colly.Debugger(&debug.LogDebugger{}),
+	)
+
+	// Limit the number of threads started by colly to two
+	// when visiting links which domains' matches "*httpbin.*" glob
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*httpbin.*",
+		Parallelism: 2,
+		//Delay:      5 * time.Second,
+	})
+
+	// Start scraping in five threads on 
+	for i := 0; i < 5; i++ {
+		c.Visit(fmt.Sprintf("%s?n=%d", url, i))
+	}
+	// Wait until threads are finished
+	c.Wait()
+}
