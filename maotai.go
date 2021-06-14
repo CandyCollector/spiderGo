f3d3e622e3df0ebf3327d38850564f517049d078
e <tuanzhang87@gmail.com>
2021-06-15 00:57:26 +0800

    修改 maotai.go 文件

--- maotai.go
+++ maotai.go
@@ -21,14 +21,14 @@ func main(){
 		// 域名过滤 支持正则
 		// https://finance.sina.com.cn/realstock/company/sh600519/nc.shtml
 		colly.URLFilters(
-			regexp,MustCompile("https://finance\\.sina\\.com\\.cn/realstock/company/^sh\\d{1,6}/\\.nc\\.shtml"),	
+			regexp.MustCompile("https://finance\\.sina\\.com\\.cn/realstock/company/^sh\\d{1,6}/\\.nc\\.shtml"),	
 	))
 
 	extensions.RandomUserAgent(c)
 	extensions.Referer(c)
 
 	c.OnHTML("a[href]",func(e *colly.HTMLElement){
-		e.request. Visit(e.Attr("href"))
+		e.request.Visit(e.Attr("href"))
 	})
 
 	c.OnRequest(func(r *colly.Request){
@@ -36,5 +36,5 @@ func main(){
 	})
 
 	c.Visit("https://finance.sina.com.cn/realstock/company")
-
+	fmt.Printf("花费时间:%s",time.Since(t))
 }
