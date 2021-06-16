423a18675f4ec2b91b514f017869a9660740a7e1
thinkpad <635079909@qq.com>
2021-06-16 11:00:41 +0800

    测试 maotai.go

--- maotai.go
+++ maotai.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/debug"
 	"github.com/gocolly/colly/v2/extensions"
-	
 )
 
-func main(){
+func main() {
+	var name string
 	t := time.Now()
 	c := colly.NewCollector(
 		// 设置异步请求
@@ -20,26 +20,37 @@ func main(){
 		colly.Debugger(&debug.LogDebugger{}),
 		// 域名过滤 支持正则
 		// https://finance.sina.com.cn/realstock/company/sh600519/nc.shtml
-		colly.URLFilters(
-			regexp.MustCompile("https://finance\\.sina\\.com\\.cn/realstock/company/^sh\\d{1,6}/\\.nc\\.shtml"),	
-	))
-	//使用扩展插件 
+		// colly.URLFilters(
+		// 	regexp.MustCompile("^(http://quote\\.eastmoney\\.com)/sh\\d{1,6}\\.html"),
+		// )
+	)
+	//使用扩展插件
 	extensions.RandomUserAgent(c)
 	extensions.Referer(c)
 
-	c.OnHTML("a[href]",func(e *colly.HTMLElement){
-		e.Request.Visit(e.Attr("href"))
+	//获取页面数据
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.DOM.Each(func(i int, selection *goquery.Selection) {
+			// #name
+			name := selection.Find("header-title-h1 f1").First().Text()
+
+			fmt.Println("输出测试")
+			fmt.Println(name)
+		})
 	})
 
-	c.OnRequest(func(r *colly.Request){
-		fmt.Println("Visting",r.URL)
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visting", r.URL)
+
 	})
 
 	c.OnError(func(response *colly.Response, err error) {
-        fmt.Println(err)
-    })
+		fmt.Println(err)
+	})
 
+	// http://quote.eastmoney.com/sh600519.html
 
-	c.Visit("https://finance.sina.com.cn/realstock/company")
-	fmt.Printf("花费时间:%s",time.Since(t))
+	c.Visit("http://quote.eastmoney.com/sh600519.html")
+	fmt.Println(name)
+	fmt.Printf("花费时间:%s", time.Since(t))
 }
