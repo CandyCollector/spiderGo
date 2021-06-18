ec856be91109b7395041a430d5c8497094ea33fc
e <tuanzhang87@gmail.com>
2021-06-19 00:05:48 +0800

    调整线程数 100

--- maotai.go
+++ maotai.go
@@ -31,7 +31,7 @@ func main() {
 	// when visiting links which domains' matches "*httpbin.*" glob
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*http.*",
-		Parallelism: 500,
+		Parallelism: 100,
 		//Delay:      5 * time.Second,
 	})
 
