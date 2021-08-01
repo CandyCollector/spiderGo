536bfdaf7d0235d3e57afdc1a0f749d08d9388e5
thinkpad <635079909@qq.com>
2021-08-01 19:25:00 +0800

    修改为具体数据的 url

--- maotai.go
+++ maotai.go
@@ -5,7 +5,6 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/debug"
 	"github.com/gocolly/colly/v2/extensions"
@@ -39,21 +38,22 @@ func main() {
 	// body > div.qphox.header_title.mb7  <h2 class="header-title-h2 fl" id="name">贵州茅台</h2>
 	// #price9
 	// //*[@id="day"]  #price9
+	// c.OnHTML("body", func(e *colly.HTMLElement) {
 
-	c.OnHTML("body", func(e *colly.HTMLElement) {
+	// 	// test := e.DOM.Find("/html/body/div[10]/div[2]/span[1]").Text()
+	// 	// fmt.Println("test:", test)
+	// 	e.DOM.Each(func(i int, selection *goquery.Selection) {
+	// 		// 	// name := selection.Find("#name").Text()
+	// 		// 	// code := selection.Find("#code").Text()
+	// 		// 	// time := selection.Find("#day").Text()
+	// 		// 	// price := selection.Find("#gt1").Text()
+	// 		fmt.Println(selection.Text())
+	// 	})
 
-		test := e.DOM.Find("#price9").Eq(0).Text()
-		e.DOM.Each(func(i int, selection *goquery.Selection) {
-			name := selection.Find("#name").Text()
-			code := selection.Find("#code").Text()
-			time := selection.Find("#hqday").Text()
-			price := selection.Find("#gt1").Text()
-			if len(name) != 0 {
-				fmt.Println(name, ":", code, ":", time, ":", price, "test:", test)
-			}
+	// })
 
-		})
-
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.Ctx.Get("url"))
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -94,7 +94,7 @@ func main() {
 
 	// }
 
-	c.Visit("http://quote.eastmoney.com/sh000001.html")
+	c.Visit("http://push2.eastmoney.com/api/qt/stock/get?ut=fa5fd1943c7b386f172d6893dbfba10b&invt=2&fltt=2&fields=f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f163,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f262,f263,f264,f267,f268,f250,f251,f252,f253,f254,f255,f256,f257,f258,f266,f269,f270,f271,f273,f274,f275,f127,f199,f128,f193,f196,f194,f195,f197,f80,f280,f281,f282,f284,f285,f286,f287,f292&secid=1.600519&cb=jQuery112406636451664591729_1627800779195&_=1627800779211")
 	c.Wait()
 	fmt.Printf("花费时间:%s", time.Since(t))
 }
