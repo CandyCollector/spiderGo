46eff6a55ccc9fd6f6a2335be5325ca168e72a7d
e <tuanzhang87@gmail.com>
2021-08-06 01:10:56 +0800

    配置数据库链接

--- maotai.go
+++ maotai.go
@@ -38,10 +38,10 @@ func main() {
 	// 	Addrs:     []string{"120.77.*.*:27017"}, //远程(或本地)服务器地址及端口号
 	// 	Direct:    false,
 	// 	Timeout:   time.Second * 1,
-	// 	Database:  "admin", //数据库
+	// 	Database:  "spidergo", //数据库
 	// 	Source:    "admin",
-	// 	Username:  "root",
-	// 	Password:  "root",
+	// 	Username:  "admin",
+	// 	Password:  "123456",
 	// 	PoolLimit: 4096, // Session.SetPoolLimit
 	// }
 	// session, err := mgo.DialWithInfo(dialInfo)
@@ -76,9 +76,9 @@ func main() {
 	// 处理接口返回 json 数据
 	c.OnResponse(func(r *colly.Response) {
 		//截取使用的数据
-		fmt.Println(string(r.Body))
+		// fmt.Println(string(r.Body))
 		rel := string(r.Body)[54 : len(r.Body)-1]
-		fmt.Println(rel)
+		// fmt.Println(rel)
 
 		//解析 json 数据
 		jue := new(data)
@@ -127,7 +127,6 @@ func main() {
 	}
 
 	// http://push2.eastmoney.com/api/qt/stock/get?ut=fa5fd1943c7b386f172d6893dbfba10b&invt=2&fltt=2&fields=f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f163,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f262,f263,f264,f267,f268,f250,f251,f252,f253,f254,f255,f256,f257,f258,f266,f269,f270,f271,f273,f274,f275,f127,f199,f128,f193,f196,f194,f195,f197,f80,f280,f281,f282,f284,f285,f286,f287,f292&secid=1.600519&_=1628157973294
-	// url := "http://push2.eastmoney.com/api/qt/stock/get?ut=fa5fd1943c7b386f172d6893dbfba10b&invt=2&fltt=2&fields=f43,f44,f51,f47,f60,f45,f52,f50,f48,f86,f58,f57&secid=1.600519&_=1628144331000"
 	// fmt.Println(url)
 	// c.Visit(url)
 	c.Wait()
